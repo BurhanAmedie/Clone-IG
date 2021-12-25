56ef96176737a575270f3e6697e7278480360cd4
Dean Barber <55028983+BurhanAmedie@users.noreply.github.com>
2021-12-25 14:23:19 -0500

    Update Suggestions.js

--- instegram/components/Suggestions.js
+++ instegram/components/Suggestions.js
@@ -22,7 +22,7 @@ function Suggestions() {
         
         {suggestions.map( (profile) => (
             <div kwey ={profile.id} className="flex items-center justify-between mt-3">
-                <img alt="" className="w-10 h-10 rounded-full p-[2px]" src={profile.avatar} />
+                <img alt="" className="w-10 h-10 rounded-full p-[2px]" src= "https://firebasestorage.googleapis.com/v0/b/instegram-83756.appspot.com/o/posts%2FRaX48qlIBChGiaLdbn17%2Fimage?alt=media&token=3149f2e6-e616-4d66-a244-95923dd091e4" />
                 <div className="flex-1 ml-4">
                     <h2 className="font-semibold text-sm">{profile.username}</h2>
                     <h3 className="text-sm text-gray-400"> Works at {profile.company.name}</h3>
